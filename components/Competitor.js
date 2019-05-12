62f7c4c716913da499ce4763e8be709eca79423c
Nicolás Vera <nicolasalevera98@gmail.com>
2019-05-12 16:31:41 -0300

    Small fixes in two components

--- components/Competitor.js
+++ components/Competitor.js
@@ -1,8 +1,8 @@
 import SVG from './utils/SVG';
 import {BodyCopy} from './utils/Typography';
 
-const Competitor = ({icon, name = ''}) => (
-  <div css={{'text-align':'center'}}>
+const Competitor = ({icon, name = '', ...props}) => (
+  <div css={{'text-align':'center'}} {...props}>
     <SVG src={'/static/icons/'+icon+'.svg'} svgStyle={{paddingBottom:'8px'}}/>
     <BodyCopy>{name}</BodyCopy>
   </div>
